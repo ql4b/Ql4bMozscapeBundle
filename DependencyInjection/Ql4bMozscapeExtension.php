01312dbc1da802a8e3d753f3ed9380e5a1220b85
Carlo D'Ambrosio <carlo@ql4b.net>
2013-06-09 16:58:06 +0200

    Initial commit

---
+++ DependencyInjection/Ql4bMozscapeExtension.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ql4b\Bundle\MozscapeBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+
+class Ql4bMandrillExtension extends Extension {
+    
+	public function load(array $configs, ContainerBuilder $container){
+		
+		$loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+		$loader->load('services.xml');
+		
+		$configuration = new Configuration();
+		$config = $this->processConfiguration($configuration, $configs);
+		
+		$container->setParameter('mozscape.client.endpoint', $config['endpoint']);
+		$container->setParameter('mozscape.client.accessId', $config['accessId']);
+		
+	}
+}
---
+++ Ql4bMozscapeBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Ql4b\Bundle\MozscapeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Ql4bMozscapeBundle extends Bundle {
+	
+}
