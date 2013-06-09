7972d3186487c5df5e735595561f9cf5fe4318b6
Carlo D'Ambrosio <carlo@ql4b.net>
2013-06-09 17:05:41 +0200

    Added secretKey

--- Service/Api/Client.php
+++ Service/Api/Client.php
@@ -1,7 +1,55 @@
 <?php
 namespace Ql4b\Bundle\MozscapeBundle\Service\Api;
 
+use Zend\Http\Client as HttpClient;
+use Zend\Http\Request;
+
 class Client
 {
+    /**
+     * @var string
+     */
+    private $apiEndipoint;
+
+    /**
+     * @var string
+     */
+    private $accessId;
+    
+    /**
+     * @var string
+     */
+    private $secretKey;
+    
+    /**
+     * @var HttpClient
+     */
+    private static $httpClient;
+    
+    /**
+     * @param string $apiEndpoint
+     * @param string $accessId
+     */
+    public function __construct($apiEndpoint, $accessId, $secretKey){
+    
+        $this->apiEndpoint = $apiEndpoint;
+        $this->accessId = $accessId;
+        $this->secretKey = $secretKey;
+    
+    }
+    
+    /**
+     * @return HttpClient
+     */
+    private static function getHttpClient(){
+    
+        if (!isset(self::$httpClient)){
+            self::$httpClient = new HttpClient();
+        }
+    
+        return self::$httpClient;
+    
+    }
+    
     
 }
